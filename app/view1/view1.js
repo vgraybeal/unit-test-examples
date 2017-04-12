7662f461750587e92477f85784768d017f61bcc9
Valerie Graybeal <valerie.graybeal@pointsource.com>
2017-04-12 16:03:58 -0400

    Working on example of Non-Deterministic code

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -5,35 +5,32 @@ angular.module('myApp.view1', ['ngRoute'])
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {
     templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
+    controller: 'View1Ctrl',
+    controllerAs: 'vm'
   });
 }])
 
-.controller('View1Ctrl', ['$scope',
-  function($scope) {
+.controller('View1Ctrl', [
+  function() {
+    var vm = this;
 
-    $scope.updateTimeOfDayDisplay = function () {
-      $scope.timeOfDayMessage = $scope.getTimeOfDay();
-    }
-
-    $scope.getTimeOfDay = function () {
+    vm.updateTimeOfDayDisplay = function () {
       var time = new Date();
 
       var hour = time.getHours();
 
       if (hour >= 0 && hour < 6 || hour > 18)
       {
-          return "Night";
+          vm.timeOfDayMessage = "Night";
       }
       if (hour >= 6 && hour < 12)
       {
-          return "Morning";
+          vm.timeOfDayMessage = "Morning";
       }
       if (hour >= 12 && hour < 18)
       {
-          return "Afternoon";
+          vm.timeOfDayMessage = "Afternoon";
       }
-      return 
     }
 
   }
--- app/view1/view1_test.js
+++ app/view1/view1_test.js
@@ -5,12 +5,33 @@ describe('myApp.view1 module', function() {
   beforeEach(module('myApp.view1'));
 
   describe('view1 controller', function(){
+    var view1Ctrl;
 
-    it('should ....', inject(function($controller) {
-      //spec body
-      var view1Ctrl = $controller('View1Ctrl');
-      expect(view1Ctrl).toBeDefined();
+    beforeEach(inject(function ($controller, $rootScope) {
+      var $scope = $rootScope.$new();
+      view1Ctrl = $controller('View1Ctrl', {
+        $scope: $scope
+      });
     }));
+
+
+    it('should update time of day to night', function () {
+      view1Ctrl.updateTimeOfDayDisplay()
+
+      expect(view1Ctrl.timeOfDayMessage).toBe('Night');
+    })
+
+    it('should update time of day to morning', function () {
+      view1Ctrl.updateTimeOfDayDisplay()
+
+      expect(view1Ctrl.timeOfDayMessage).toBe('Morning');
+    })
+
+    it('should update time of day to afternoon', function () {
+      view1Ctrl.updateTimeOfDayDisplay();
+
+      expect(view1Ctrl.timeOfDayMessage).toBe('Afternoon');
+    })
 
   });
 });
