b4561df9190907b74064ba86de86b49d6fa19ce5
Valerie Graybeal <valerie.graybeal@pointsource.com>
2017-04-27 15:30:17 -0400

    Reformatted SmartHomeService

--- app/views/timeOfDay/smartHome.service.js
+++ app/views/timeOfDay/smartHome.service.js
@@ -2,49 +2,43 @@
 
 angular.module('myApp.timeOfDay')
 
+  .service('SmartHomeService', ['LightService',
+    function (LightService) {
+      var lastMotionTime;
 
-.service('SmartHomeService', ['LightService',
-  function(LightService) {
-    var lastMotionTime;
+      this.getTimeOfDay = function () {
+        var time = new Date();
+        var hour = time.getHours();
 
-
-    this.getTimeOfDay = function () {
-      var time = new Date();
-      var hour = time.getHours();
-
-      if (hour >= 0 && hour < 6 || hour > 18)
-      {
+        if (hour >= 0 && hour < 6 || hour > 18) {
           return "Night";
-      }
-      if (hour >= 6 && hour < 12)
-      {
+        }
+        if (hour >= 6 && hour < 12) {
           return "Morning";
-      }
-      if (hour >= 12 && hour < 18)
-      {
+        }
+        if (hour >= 12 && hour < 18) {
           return "Afternoon";
+        }
       }
+
+      this.actuateLights = function (motionDetected) {
+        if (motionDetected) {
+          lastMotionTime = new Date();
+        }
+
+        var diffMs = (new Date() - lastMotionTime);
+        var diffMins = Math.round(((diffMs % 86400000) % 3600000) / 60000); // minutes
+        var timeOfDay = this.getTimeOfDay();
+
+        // If motion was detected in the evening or at night, turn the light on.
+        if (motionDetected && (timeOfDay === "Night")) {
+          LightService.turnLightsOn();
+        }
+        // If no motion is detected for one minute, or if it is morning or day, turn the light off.
+        else if (diffMins > 1 || (timeOfDay == "Morning" || timeOfDay == "Afternoon")) {
+          LightService.turnLightsOff();
+        }
+      }
+
     }
-
-    this.actuateLights = function (motionDetected) {
-      if (motionDetected) {
-        lastMotionTime = new Date();
-      }
-
-      var diffMs = (new Date() - lastMotionTime);
-      var diffMins = Math.round(((diffMs % 86400000) % 3600000) / 60000); // minutes
-      var timeOfDay = this.getTimeOfDay();
-
-      // If motion was detected in the evening or at night, turn the light on.
-      if (motionDetected && (timeOfDay === "Night")) {
-        LightService.turnLightsOn();
-      } 
-      // If no motion is detected for one minute, or if it is morning or day, turn the light off.
-      else if (diffMins > 1 || (timeOfDay == "Morning" || timeOfDay == "Afternoon"))
-      {
-        LightService.turnLightsOff();   
-      }
-    }
-
-  }
-]);
+  ]);
